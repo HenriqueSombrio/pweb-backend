4fa5a35758ffad7ec79aa28a9ccc58b6851511b7
Henrique Sombrio <henriquesombrio3@gmail.com>
2021-05-05 21:44:51 -0300

    Adicionado os services e controllers de pessoas

--- Spring/src/main/java/com/timing/teste/api/PessoaController.java
+++ Spring/src/main/java/com/timing/teste/api/PessoaController.java
@@ -1,9 +1,31 @@
 package com.timing.teste.api;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.timing.teste.domain.Pessoa;
+import com.timing.teste.services.PessoaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/pessoas")
 public class PessoaController {
+
+    @Autowired
+    PessoaService service;
+
+    @GetMapping
+    public List<Pessoa> listaTodas(){
+        return service.listaPessoa();
+    }
+
+    @GetMapping("/id/{id}")
+    public Pessoa listaPorId(@PathVariable(name = "id") int id){
+        return service.buscaPorId(id);
+    }
+
+    @PostMapping
+    public String mostraNovaPessoa(@RequestBody Pessoa novaPessoa){
+        return novaPessoa.toString();
+    }
 }
--- Spring/src/main/java/com/timing/teste/services/PessoaService.java
+++ Spring/src/main/java/com/timing/teste/services/PessoaService.java
@@ -2,6 +2,8 @@ package com.timing.teste.services;
 
 import com.timing.teste.domain.Pessoa;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,5 +17,21 @@ public class PessoaService {
         lista.add(new Pessoa(3, "Wendel", 1));
 
         return lista;
+    }
+
+    public Pessoa buscaPorId(int id){
+        List<Pessoa> lista = new ArrayList<Pessoa>();
+        lista.add(new Pessoa(1, "Henrique", 24));
+        lista.add((new Pessoa(2, "Larissa", 20)));
+        lista.add(new Pessoa(3, "Wendel", 1));
+
+        if (id == 1)
+            return lista.get(0);
+        if (id==2)
+            return lista.get(1);
+        if (id==3)
+            return lista.get((2));
+
+        return null;
     }
 }
