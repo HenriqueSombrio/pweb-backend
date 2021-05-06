8fe241c1daed0a142d90ed97d4396729547aae52
Henrique Sombrio <henriquesombrio3@gmail.com>
2021-05-05 21:50:37 -0300

    Adicionada listagem de carros.

--- Spring/src/main/java/com/timing/teste/api/CarroController.java
+++ Spring/src/main/java/com/timing/teste/api/CarroController.java
@@ -1,5 +1,25 @@
 package com.timing.teste.api;
 
+import com.timing.teste.domain.Carro;
+import com.timing.teste.services.CarroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/carros")
 public class CarroController {
+
+    @Autowired
+    CarroService service;
+
+    @GetMapping
+    public List<Carro> listaCarros(){
+        return service.listaCarros();
+    }
+
 
 }
